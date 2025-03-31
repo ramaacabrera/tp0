fdb28d4b630286b92e632de5f2dfdcc90f8ba11f
rama <ramacabrera16@gmail.com>
2025-03-31 01:08:01 +0000

    tp0

--- client/src/client.h
+++ client/src/client.h
@@ -11,10 +11,70 @@
 #include "utils.h"
 
 
-t_log* iniciar_logger(void);
-t_config* iniciar_config(void);
-void leer_consola(t_log*);
-void paquete(int);
-void terminar_programa(int, t_log*, t_config*);
+t_log* iniciar_logger(void){
+    t_log* logger_nuevo = log_create("cliente.log","cliente_logger", true, log_level_from_string("info"));
+    if(logger_nuevo == NULL) {
+        perror("Algo salio mal al crear el log");
+        abort();
+    }
+	return logger_nuevo;
+}
+t_config* iniciar_config(void)
+{
+    t_config* nueva_config = config_create("/home/utnso/tp0/client/cliente.config");
+    if(nueva_config == NULL){
+        perror("algo salio mal creando la config");
+        abort();
+    }
+    return nueva_config;
+}
+
+void leer_consola(t_log* logger)
+{
+	char* leido;
+
+	// La primera te la dejo de yapa
+	leido = readline("> ");
+
+	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
+
+    while(strcmp(leido, "") != 0){
+        log_info(logger, ">> %s", leido);
+        leido = readline("> ");
+    }
+	// ¡No te olvides de liberar las lineas antes de regresar!
+    free(leido);
+}
+
+void paquete(int conexion)
+{
+	// Ahora toca lo divertido!
+	char* leido;
+	t_paquete* paquete;
+
+	// Leemos y esta vez agregamos las lineas al paquete
+    leido = readline(">>");
+    paquete = crear_paquete();
+    while(strcmp(leido, "") != 0){
+        agregar_a_paquete(paquete, leido, strlen(leido)+1);
+
+        leido = readline(">>");
+    }
+
+    enviar_paquete(paquete, conexion);
+	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
+	free(leido);
+    eliminar_paquete(paquete);
+}
+
+void terminar_programa(int conexion, t_log* logger, t_config* config)
+{
+	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
+	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	log_destroy(logger);
+    config_destroy(config);
+    close(conexion);
+}
 
 #endif /* CLIENT_H_ */
+
--- server/src/utils.c
+++ server/src/utils.c
@@ -5,38 +5,56 @@ t_log* logger;
 int iniciar_servidor(void)
 {
 	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
+	// assert(!"no implementado!");
 
-	int socket_servidor;
+	int fd_escucha;
+	int err;
 
-	struct addrinfo hints, *servinfo, *p;
+	struct addrinfo hints, *server_info, *p;
 
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
-	getaddrinfo(NULL, PUERTO, &hints, &servinfo);
+	err = getaddrinfo(NULL, PUERTO, &hints, &server_info);
+
+	if(err == -1){
+		error_show("error al crear el socket del servidor");
+		abort();
+	}
 
 	// Creamos el socket de escucha del servidor
 
+	fd_escucha = socket(server_info->ai_family,
+                        server_info->ai_socktype,
+                        server_info->ai_protocol);
+
 	// Asociamos el socket a un puerto
+
+	err = setsockopt(fd_escucha, SOL_SOCKET, SO_REUSEPORT, &(int){1}, sizeof(int));
+
+	err = bind(fd_escucha, server_info->ai_addr, server_info->ai_addrlen);
+
 
 	// Escuchamos las conexiones entrantes
 
-	freeaddrinfo(servinfo);
+	err = listen(fd_escucha, SOMAXCONN);
+
+	freeaddrinfo(server_info);
 	log_trace(logger, "Listo para escuchar a mi cliente");
 
-	return socket_servidor;
+	return fd_escucha;
 }
 
 int esperar_cliente(int socket_servidor)
 {
 	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
+	// assert(!"no implementado!");
 
 	// Aceptamos un nuevo cliente
-	int socket_cliente;
+	int socket_cliente = accept(socket_servidor, NULL, NULL);
+
 	log_info(logger, "Se conecto un cliente!");
 
 	return socket_cliente;
@@ -69,7 +87,7 @@ void recibir_mensaje(int socket_cliente)
 {
 	int size;
 	char* buffer = recibir_buffer(&size, socket_cliente);
-	log_info(logger, "Me llego el mensaje %s", buffer);
+	log_info(logger, "Me llego el siguiente mensaje: %s", buffer);
 	free(buffer);
 }
 
@@ -94,3 +112,5 @@ t_list* recibir_paquete(int socket_cliente)
 	free(buffer);
 	return valores;
 }
+
+
